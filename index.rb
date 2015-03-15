5e0cea32bbb96d5c472fa310a54697479dd2bb09
Tyler Nappy <tylernappy@gmail.com>
2015-03-15 17:10:02 -0400

    now adds, deletes, and corrects menu items

--- index.rb
+++ index.rb
@@ -72,16 +72,26 @@ def correct_breakdown(body, meal)
       dish = meal.dishes.find_by_bin_key(vals.first)
       if dish.nil?
          #add new item
+         meal.dishes.create!({bin_key: meal.dishes.last.bin_key.next, item: vals[0], price: vals[1]})
       else
          #correct item
          if vals.length == 3
             #correct everything
+            dish.update_attributes(item: vals[1], price: vals[2])
          else
-            #correct only name or price
-            if $letters.include?(vals.last.split("").first.downcase) #check first character of second (last) entry
-               #correct the name
+            #correct only name or price, or delete item
+            if vals.last == "delete"
+               #delete dish
+               dish.destroy
             else
-               #correct the price
+               #correct only name or price
+               if $letters.include?(vals.last.split("").first.downcase) #check first character of second (last) entry
+                  #correct the name
+                  dish.update_attributes(item: vals.last)
+               else
+                  #correct the price
+                  dish.update_attributes(price: vals.last)
+               end
             end
          end
       end
